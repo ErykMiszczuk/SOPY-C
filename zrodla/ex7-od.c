8a3e521201d881f3759d0cd9758378b4e7869095
Eryk Miszczuk <s426197@shell.wmi.amu.edu.pl>
2017-12-12 17:02:50 +0100

    12.12.2017cwiczenia

--- zrodla/ex7-od.c
+++ zrodla/ex7-od.c
@@ -4,9 +4,9 @@ int main(){
 
 int c, n;
 
-n = 1;
-while ((c = getchar()) != EOF) {
-  printf("%3o", c);
+n = 1;                 // EOF = -1
+while ((c = getchar()) != EOF) { // makro getchar() pobiera wartości ze standardowego urządzenia wejścia
+  printf("%3o", c); 
   if (n++ >= 10) {
     n = 1;
     printf("\n");
--- zrodla/ex9-cat.c
+++ zrodla/ex9-cat.c
@@ -6,11 +6,11 @@ main()
 #define BLOCK 512
 
 copyfile(fd1, fd2)
-int fd1, fd2;   
+int fd1, fd2;
 {
   char buffer[BLOCK];
   int n;
-
+                   //deskrryptor, tablica, ile mamy odczytać znaków z pliku
   while ((n = read(fd1, buffer, BLOCK)) > 0)
     write(fd2, buffer, n);
 }
